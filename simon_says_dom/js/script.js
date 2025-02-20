cf4db635316e7a7d341dcfa349406cbe2c88e09d
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-20 15:15:38 +0100

    add pseudocode

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,19 +1,39 @@
 
 
 const countdownEl = document.getElementById(`countdown`)
-console.log(countdownEl);
+//console.log(countdownEl);
 
 const instructionsEl = document.getElementById(`instructions`)
-console.log(instructionsEl);
+//console.log(instructionsEl);
 
 const numbersListEl = document.getElementById(`numbers-list`)
-console.log(numbersListEl);
+//console.log(numbersListEl);
 
 const answersFormEl = document.getElementById(`answers-form`)
-console.log(answersFormEl);
+//console.log(answersFormEl);
 
 const buttonEl = document.querySelector(`.btn`)
-console.log(buttonEl);
+//console.log(buttonEl);
 
 const messageEl = document.getElementById(`message`)
-console.log(messageEl);
+//console.log(messageEl);
+
+
+//generate 5 numbers with math.random
+
+//30s countdown
+
+//make them disappear
+
+//show inputs
+
+//get inputs
+
+//check inputs with original numbers
+
+//tell users original numbers
+
+//tell users number of correct guesses
+
+//tell users chosen numbers
+
