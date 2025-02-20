54f1b11234070d87a7dcbb5aa4667c3bb0ba4ca4
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-20 17:11:07 +0100

    forgot to push, add main code

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -12,6 +12,12 @@ const numbersListEl = document.getElementById(`numbers-list`)
 const answersFormEl = document.getElementById(`answers-form`)
 //console.log(answersFormEl);
 
+const inputGroupEl = document.getElementById(`input-group`)
+//console.log(inputGroupEl);
+
+const inputElChildren = inputGroupEl.children
+console.log(inputElChildren);
+
 const buttonEl = document.querySelector(`.btn`)
 //console.log(buttonEl);
 
@@ -19,20 +25,107 @@ const messageEl = document.getElementById(`message`)
 //console.log(messageEl);
 
 
-//generate 5 numbers with math.random
+let seconds = 5; //5s for debugging, should be 30
+const userNumbers = [];
+const randomNumbers = [];
+const correctNumbers = [];
+//generate 5 numbers with math.random and show them in the ul
+
+for (let i = 0; i < 5; i++) {
+    randomNumbers[i] = Math.floor(Math.random() * 50)
+    numbersListEl.insertAdjacentHTML('beforeend', `<li class="list-item" </li>${randomNumbers[i]}</li>\n`)
+}
+console.log(`these are the randomNumbers ${randomNumbers}`);
 
 //30s countdown
+const countdownTimer = setInterval(function () {
+    seconds--;
+    countdownEl.innerHTML = `${seconds} secondi rimanenti!`
+    if (seconds == 1) {
+        countdownEl.innerHTML = `${seconds} secondo rimanente!`
+    } else if (seconds == 0) {
+        //make them disappear
+        hideOutput()
+        displayInputs()
+        clearInterval(countdownTimer)
+    }
 
-//make them disappear
+
+}, 1000)
+
+
+
+
+
+//functions
+function hideOutput() {
+    countdownEl.innerHTML = `${seconds} secondi rimanenti!`
+    instructionsEl.innerText = "Ora indovina i numeri! Puoi scriverli in qualsiasi ordine!"
+    numbersListEl.classList.add("d-none")
+
+}
 
 //show inputs
 
-//get inputs
+function displayInputs() {
+    answersFormEl.classList.remove("d-none")
+}
 
+//get inputs
+buttonEl.addEventListener(`click`, getInputs)
+
+function getInputs(e) {
+    e.preventDefault()
+    for (let i = 0; i < 5; i++) {
+        userNumbers[i] = Number(inputElChildren[i].value)
+    }
+    console.log(`these are the userNumbers   ${userNumbers}`);
+    checkUserValues()
+}
 //check inputs with original numbers
+
+function checkUserValues() {
+
+    for (let i = 0; i < 5; i++) {
+        //console.log(`this is inside the loop but outside the if! i'm the index of the ${i} element! and this is the index! ${randomNumbers.indexOf(userNumbers[i])}`);
+
+        if (randomNumbers.indexOf(userNumbers[i]) > -1) {
+            //console.log(`i'm in the verified check loop! and this is the index of the current (${i}) cycle, this is the index of the number in the random generated array!${randomNumbers.indexOf(userNumbers[i])}`);
+
+            correctNumbers.push(userNumbers[i])
+            inputElChildren[i].classList.add("bg-success")
+        } else {
+            inputElChildren[i].classList.add("bg-danger")
+        }
+
+    }
+
+    console.log(`these are the correctNumbers ${correctNumbers}`);
+    displayOutput()
+}
 
 //tell users original numbers
 
+function displayOutput() {
+    buttonEl.disabled = true;
+    numbersListEl.classList.remove("d-none")
+    for (let i = 0; i < 5; i++) {
+        inputElChildren[i].readOnly = true;
+    }
+
+    if (correctNumbers.length == 0) {
+        countdownEl.innerHTML = `Nemmeno uno? Sei un disastro!`
+    } else if (correctNumbers.length == 1) {
+        countdownEl.innerHTML = `puoi fare di meglio, credo in te!`
+    } else if (correctNumbers.length == 2) {
+        countdownEl.innerHTML = `bene, ma non benissimo, ritenta!`
+    } else if (correctNumbers.length == 3) {
+        countdownEl.innerHTML = `quasi! ce la puoi fare!`
+    } else if (correctNumbers.length == 4) {
+        countdownEl.innerHTML = `Congratulazioni! li hai scritti tutti!`
+    }
+
+}
 //tell users number of correct guesses
 
 //tell users chosen numbers
